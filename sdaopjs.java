54f567f06e656da08cf8ae88170c56d7f277b6b0
NikolaSN <74970423+NikolaSN@users.noreply.github.com>
2020-11-27 18:14:48 +0200

    iztrih i napisah neshta

--- sdaopjs.java
+++ sdaopjs.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class sdaopjs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner input =new Scanner(System.in);
 		int n;
 		n=input.nextInt();
@@ -16,7 +16,7 @@ public class sdaopjs {
 		}
 		int max = arr[0];
 		int index = 0;
-		for(int i=0; i<n; i++) {
+		for(int i=1; i<n; i--) {
 			if(max< arr[i]) {
 				index=i;
 				max=arr[i];
